d51b5f576ce299cb7be269885b5f5af1ed020be0
Fro1s <matheuscosta1150@gmail.com>
2025-04-21 12:12:35 -0300

    refactor(movies): enhance movie listing logic with pagination control

--- src/lib/hooks/useMovies.ts
+++ src/lib/hooks/useMovies.ts
@@ -1,19 +1,20 @@
-"use client"
+"use client";
 
-import { ApiResponse, ISearch } from "../types";
-import { moviesService } from "../services/movies";
 import { useQuery } from "@tanstack/react-query";
+import { moviesService } from "../services/movies";
+import { ApiResponse, ISearch } from "../types";
 
 export function useMovies({ page, per_page, search }: ISearch) {
+    const disablePagination = !!search; 
 
     const { data: movies, isLoading: isLoadingMovies, isError: isErrorMovies } = useQuery<ApiResponse>({
         queryKey: ['get-movies', page, per_page, search],
-        queryFn: () => moviesService.list({ page, per_page, search }),
-    })
+        queryFn: () => moviesService.list({ page, per_page, search, disablePagination }),
+    });
 
     return {
         movies,
         isLoadingMovies,
         isErrorMovies
-    }
+    };
 }
--- src/lib/services/movies.ts
+++ src/lib/services/movies.ts
@@ -1,16 +1,18 @@
-"use client"
-import { ApiResponse, ISearch } from "../types"
-import { api } from "./api"
+import { ApiResponse, ISearch } from "../types";
+import { api } from "./api";
 
 export const moviesService = {
-    async list({ page, per_page, search }: ISearch): Promise<ApiResponse> {
-        try {
-            const response = await api.get<ApiResponse>(`/movies?page=${page}&limit=${per_page}&search=${search}`);
-            return response.data;
-        } catch (error) {
-            console.error("Error fetching movies:", error);
-            throw error;
-        }
-    }
+    async list({ page, per_page, search, disablePagination = false }: ISearch & { disablePagination?: boolean }): Promise<ApiResponse> {
+        const query = new URLSearchParams();
 
-}
+        if (!disablePagination) {
+            if (page) query.append("page", page.toString());
+            if (per_page) query.append("limit", per_page.toString());
+        } else {
+            query.append("limit", "9999"); 
+        }
+
+        const response = await api.get<ApiResponse>(`/movies?${query.toString()}`);
+        return response.data;
+    }
+};
